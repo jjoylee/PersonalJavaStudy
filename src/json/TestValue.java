a58468bf75720f177354deffdfa7bf7db76f6405
jjoylee <ekdnlt1995@gmail.com>
2018-07-04 11:08:00 +0900

    TestValue object

---
+++ src/json/TestValue.java
@@ -0,0 +1,47 @@
+package json;
+
+public class TestValue {
+	private String name;
+	private int age;
+	private double height;
+	private float weight;
+	private boolean married;
+	private String cutomerName;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	public float getWeight() {
+		return weight;
+	}
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	public boolean isMarried() {
+		return married;
+	}
+	public void setMarried(boolean married) {
+		this.married = married;
+	}
+	public String getCutomerName() {
+		return cutomerName;
+	}
+	public void setCutomerName(String cutomerName) {
+		this.cutomerName = cutomerName;
+	}
+	
+}
